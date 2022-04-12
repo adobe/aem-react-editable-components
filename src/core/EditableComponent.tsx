90037b8c31b93f786e6381a0c38d095d5ed9ef13
svinod <sharanyavinod90@gmail.com>
2022-04-12 19:16:09 +0200

    Remove comment

--- src/core/EditableComponent.tsx
+++ src/core/EditableComponent.tsx
@@ -9,8 +9,6 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-/* @ts-ignore */
 import React, { ReactNode, useEffect, useState } from 'react';
 import { MappedComponentProperties } from './ComponentMapping';
 import { ClassNames, Properties } from '../constants';
